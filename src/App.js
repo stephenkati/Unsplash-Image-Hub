b4a3b33d2b523d4d020dda3e5a83292e4f84917e
Stephen <stephenkatuli88@gmail.com>
2023-04-17 10:48:41 +0300

    add styles for nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Images from './components/Images';
+import Likes from './components/Likes';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Images />} />
+        <Route path="/likes" element={<Likes />} />
+      </Routes>
     </div>
   );
 }

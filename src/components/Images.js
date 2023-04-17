d1e8df30e7c3c3dd6fb5c61c9044d49fe6df179b
Stephen <stephenkatuli88@gmail.com>
2023-04-17 16:57:56 +0300

    change components names

--- src/components/Images.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Images = () => {
-  return (
-    <div>Images</div>
-  )
-}
-
-export default Images;
--- src/components/Likes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Likes = () => {
-  return (
-    <div>Likes</div>
-  )
-};
-
-export default Likes;

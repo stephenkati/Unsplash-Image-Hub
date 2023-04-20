4e7ffb75e5ce1f9057b385c923c0b76cbd6ffa3b
Stephen <stephenkatuli88@gmail.com>
2023-04-20 14:54:26 +0300

    add icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../App.css';
+import { IoIosArrowBack, IoIosSettings } from 'react-icons/io';
+import { FaMicrophone } from 'react-icons/fa';
 
 const Navbar = () => (
     <div className="links">
+      <NavLink to="/">
+        <IoIosArrowBack className="nav-links" />
+      </NavLink>
       <NavLink to="/" className="images-link nav-links">Collections</NavLink>
-      <NavLink to="/photos" className="likes-link nav-links">Photos</NavLink>
+      <div>
+        <FaMicrophone className="nav-links" />
+        <IoIosSettings className="nav-links" />
+      </div>
+      {/* <NavLink to="/photos" className="likes-link nav-links">Photos</NavLink> */}
     </div>
 );
 

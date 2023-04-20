c6c72428cb11d2c69884095b2f6af46df1241d8b
Stephen <stephenkatuli88@gmail.com>
2023-04-20 14:55:37 +0300

    update photo js

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { MdOutlineArrowCircleRight } from 'react-icons/md';
 
 const Photos = () => {
   const photos = useSelector((state) => state.photos.photos);
   console.log(photos)
 
   return (
-    <div>
+    <div className="photos-list">
       {
         photos.map((photo) => (
           <li key={photo.id}>
-            <span> likes = {photo.likes} </span>
-            <span>
+            <span className="description">
               {
                 photo.alt_description
               }
             </span>
-            <img src={photo.urls.regular} alt={photo.alt_description} />
+            <div className="like-redirect">
+              <span className="photo-likes"> {photo.likes} Likes </span>
+              <span>
+                <MdOutlineArrowCircleRight
+                  className="redirect"
+                />
+              </span>
+            </div>
+            {/* <img src={photo.urls.regular} alt={photo.alt_description} /> */}
           </li>
         ))
       }

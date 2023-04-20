85b42e77f24dbebbabafa7db4a911f9211918da3
Stephen <stephenkatuli88@gmail.com>
2023-04-20 20:56:18 +0300

    update details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,48 +2,77 @@ import { useSelector } from "react-redux";
 
 const Details = () => {
   const details = useSelector((state) => state.collectionDetails.collectionDetails);
-  console.log(details)
 
   return (
-    <div style={{color:"white"}} key={details.title}>
-      <div><img src={details.cover_photo.urls.regular} /> </div>
-      <div>Title: {details.title}</div>
-      {!details.description && (
-        <div>
-          Description: null
-        </div>
-      )}
-      {details.description && (
-        <div>
-          Description: {details.description}
-        </div>
-      )}
-      <div>Number of images: {details.total_photos}</div>
-      <div>
-        Tags:
-        {
-          details.tags.map((tag) => (
-            <p key={tag.id}>
-              {tag.title}
-            </p>
-          ))
-        }
+    <div style={{color:"white"}} key={details.title} className="details-page">
+      <div className="details-header">
+        <img src={details.cover_photo.urls.regular} />
+        <h2>{details.title}</h2> 
       </div>
-      <div>
-        Photos in the collection:
-        <a href={details.links.html} target="_blank" rel="noreferrer">Photos in "{details.title}" collection</a>
+      <div className="collection-stats">
+        <h3>Collection Stat's</h3>
       </div>
-      <div>Share Key: {details.share_key}</div>
-      <div>
-        Preview Photos:
-        {
-          details.preview_photos.map((photo) => (
-            <p key={photo.id}>
-              <img src={photo.urls.regular} style={{width: "10%"}} alt={details.title} />
-            </p>
-          ))
-        }
-      </div>
+      <ul className="details-list">
+        <li className="details-list-item">
+          <span>Title</span>
+          <span>{details.title}</span>
+        </li>
+        {!details.description && (
+          <li className="details-list-item">
+            <span>Description</span>
+            <span>null</span>
+          </li>
+        )}
+        {details.description && (
+          <li className="details-list-item">
+            <span>Description</span>
+            <span>{details.description}</span>
+          </li>
+        )}
+        <li className="details-list-item">
+          <span>Total photos</span>
+          <span>{details.total_photos}</span>
+        </li>
+        <li className="details-list-item tag">
+          <span>Tags</span>
+          <div className="tags">
+            {
+              details.tags.map((tag) => (
+                <div key={tag.id} >
+                  <span>{tag.title},</span>
+                </div>
+              ))
+            }
+          </div>
+        </li>
+        <li className="details-list-item">
+          <span>Photos in the collection</span>
+          <span>
+            Photos in
+            <a
+              href={details.links.html}
+              target="_blank" rel="noreferrer"
+            >
+              " {details.title} "
+            </a>
+            collection
+          </span>
+        </li>
+        <li className="details-list-item preview">
+          <span>Preview Photos</span>
+          <div className="image-container">
+            {
+              details.preview_photos.map((photo) => (
+                  <img
+                    key={photo.id}
+                    src={photo.urls.regular}
+                    alt={details.title}
+                  />
+              ))
+            }
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };

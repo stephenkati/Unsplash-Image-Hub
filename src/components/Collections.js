046b9f297789ee0982a5c68bfd1c718ca39e9ee3
stephenkati <stephenkatuli88@gmail.com>
2023-04-24 12:48:38 +0300

    update collection component

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -1,34 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { MdOutlineArrowCircleRight } from 'react-icons/md';
 import { fetchCollections } from '../redux/CollectionsSlice';
-import SingleCollection from './SingleCollection';
+import { fetchCollectionDetails } from '../redux/DetailsSlice';
 import unsplashImage from '../image/shinny-hand.png';
 
 const Collections = () => {
   const dispatch = useDispatch();
   const [search, setSearch] = useState('');
   const [page, setPage] = useState(1);
-  
+
   const handlePrevious = () => setPage(page - 1);
   const handleNext = () => setPage(page + 1);
 
   useEffect(() => {
-    dispatch(fetchCollections(page))
+    dispatch(fetchCollections(page));
   }, [dispatch, page]);
 
   const collection = useSelector((state) => state.collections.collections);
 
+  const handleClick = (id) => {
+    dispatch(fetchCollectionDetails(id));
+  };
+
   return (
     <div className="collection-container">
-        <div className="search">
-          <form>
-            <input
-              type="text"
-              placeholder="Search collection name"
-              onChange={(e) => setSearch(e.target.value)}
-            />
-          </form>
-        </div>
+      <div className="search">
+        <form>
+          <input
+            type="text"
+            placeholder="Search collection name"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
+      </div>
       <div className="collection-header">
         <img
           src={unsplashImage}
@@ -42,33 +48,64 @@ const Collections = () => {
       <div className="collection-list">
         {
           collection
-          .filter((collection) => {
-            return search.toLowerCase() === ''
-            ? collection
-            : collection.title.toLowerCase().includes(search);
-          })
-          .map((collection) => (
-            <SingleCollection key={collection.id} collection={collection} />
-          ))
+            .filter((collection) => (search.toLowerCase() === ''
+              ? collection
+              : collection.title.toLowerCase().includes(search)))
+            .map((collection) => (
+              <NavLink
+                to="/details"
+                className="collection-link"
+                key={collection.id}
+              >
+                <div
+                  role="button"
+                  tabIndex={0}
+                  onClick={() => handleClick(collection.id)}
+                  onKeyDown={() => handleClick(collection.id)}
+                  className="collection"
+                >
+                  <MdOutlineArrowCircleRight
+                    className="arrow"
+                  />
+                  <img
+                    src={collection.cover_photo.urls.small}
+                    alt={collection.title}
+                    className="collection-img"
+                  />
+                  <div className="collection-title">
+                    {collection.title}
+                  </div>
+                  <div className="cover-likes">
+                    {collection.total_photos}
+                  </div>
+                </div>
+              </NavLink>
+            ))
         }
       </div>
       <div className="page">
-        {page>1 && (
+        {page > 1 && (
           <span
+            role="button"
+            tabIndex={0}
             onClick={handlePrevious}
+            onKeyDown={handlePrevious}
           >
             Prev
           </span>
         )}
         {page}
         <span
+          role="button"
+          tabIndex={0}
           onClick={handleNext}
+          onKeyDown={handleNext}
         >
           Next
         </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Collections;
